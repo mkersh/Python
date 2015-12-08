979874b4ebb4bb93108a70f0fbdb07a9024c1c7a
MK <kershaw.mark@gmail.com>
2015-12-08 13:36:59 +0000

    Added SOAP test client

---
+++ experiment/SOAP/testClient.py
@@ -0,0 +1,16 @@
+from pysimplesoap.client import SoapClient, SoapFault
+
+# create a simple consumer
+client = SoapClient(
+    location = "http://localhost:8008/",
+    action = 'http://localhost:8008/', # SOAPAction
+    namespace = "http://example.com/sample.wsdl", 
+    soap_ns='soap',
+    ns = False)
+
+# call the remote method
+response = client.Adder(a=1, b=2)
+
+# extract and convert the returned value
+result = response.AddResult
+print(result)
