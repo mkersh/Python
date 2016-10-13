77a9c525faa779910e25a9a54e127038dc4c121c
Mark Kershaw <kershaw.mark@gmail.com>
2016-10-13 09:59:34 +0100

    Moved queens into a seperate directory

---
+++ experiment/queens/queens.py
@@ -0,0 +1,11 @@
+from itertools import permutations
+sol = 1 # extra line I added
+n = 8
+cols = range(n)
+print(cols)
+
+for vec in permutations(cols):
+    if (n == len(set(vec[i]+i for i in cols))
+          == len(set(vec[i]-i for i in cols))):
+        print("[{0}] {1}").format(sol, vec)
+        sol += 1 # extra line I added
