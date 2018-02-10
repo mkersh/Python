e2bee9f67f4394777a09205a5f4368bf8359ca25
Mark Kershaw <mkersh@Sharons-Air.lan>
2018-02-10 16:03:26 +0000

    update
    Committer: kershm <kershaw.mark@gmail.com>

--- experiment/queens/queens2.py
+++ experiment/queens/queens2.py
@@ -6,9 +6,12 @@ from stopwatch import *
 
 num = 1
 def nqueens(r,n,p):
+  # p represents a potential board position for the queens. It is a vector that represents the column positions of queens for
+  # a board with n rows and n columns. The values in the vector identify column positions for the different rows.
   global num
   s  = len(p)
   cols = range(s)
+  # This next line is performing the diagonal check on p to see if it is valid so far
   valid = s == len(set(p[i] + i for i in cols)) == len(set(p[i] - i for i in cols))
 
   count = 0
@@ -18,7 +21,7 @@ def nqueens(r,n,p):
       print("[{i}] {vec}").format(i=num,vec=p)
       num += 1
       return 1
-
+    # This next line is performing all the magic. It is doing a set difference and only considering columns not currently considered  
     for c in set(range(n)) - set(p):
         count += nqueens(r + 1, n, p + [c])
 
